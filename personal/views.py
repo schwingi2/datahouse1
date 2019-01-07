7bd8cd0d2038fd1b26689151f12c0691c08e860c
schwingi2 <albert.schwingenschloegl@gmail.com>
2019-01-07 10:09:15 +0100

    First commit for datahouse

---
+++ personal/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+def contact(request):
+	return render(request,'personal/basic.html',{'content':['If you would like to contact me, please email me.','albert.schwingenschloegl@gmail.com']})
+	
